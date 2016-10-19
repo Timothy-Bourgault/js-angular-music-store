d19091344f99793b095f96bfa295be55ea90063e
Tim Bourgault and Yusuf Qedan <tim+yusuf@>
2016-10-19 16:07:56 -0700

    update navbar menu for proper styling.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,38 +1,95 @@
 import { Component } from '@angular/core';
 import { Cd } from './cd.model';
 import { User } from './user.model';
+import { MasterCdList } from './master-cd-list.model';
 
 @Component({
   selector: 'my-app',
   template: `
+
   <div class="container">
-    <h1>Music Store</h1>
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+    <h1>Zappa's&nbsp;&nbsp;&nbsp;&nbsp;Music&nbsp;&nbsp;&nbsp;&nbsp;Shop</h1>
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div id="pipes" class="navbar-header navbar-left">
-          <label>Sort By Genre</label>
-          <select (change)="onGenreChange($event.target.value)" class="filter">
-            <option value="all" selected="selected">All Genres</option>
-            <option value="AGR">Avant-Garde-Rap</option>
-            <option value="PR">Psychadelic-Rock</option>
-            <option value="PP">Post-Psychadelic</option>
-            <option value="AE">Accapella-Experimental</option>
-            <option value="CFAGM">Cinimatic-Folk-Avant-Garde-Metal</option>
-          </select>
-          <label>Sort By Artist</label>
-          <select (change)="onArtistChange($event.target.value)" class="filter">
-            <option value="all" selected="selected">All Artists</option>
-            <option value="Pink Floyd">Pink Floyd</option>
-            <option value="Grateful Dead">Grateful Dead</option>
-            <option value="Hella">Hella</option>
-            <option value="Grails">Grails</option>
-            <option value="Busdriver">Busdriver</option>
-            <option value="Aesop Rock">Aesop Rock</option>
-            <option value="Bjork">Bjork</option>
-            <option value="Mike Patton">Mike Patton</option>
-            <option value="Secret Chiefs 3">Secret Chiefs 3</option>
-            <option value="Estradasphere">Estradasphere</option>
-          </select>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Genres<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a (click)= "onGenreChange('all')" >All Genres</a></li>
+              <li><a (click)= "onGenreChange('AGR')" >Avant-Garde-Rap</a></li>
+              <li><a (click)= "onGenreChange('PR')" >Psychadelic-Rock</a></li>
+              <li><a (click)= "onGenreChange('PP')" >Post-Psychadelic</a></li>
+              <li><a (click)= "onGenreChange('AE')" >Accapella-Experimental</a></li>
+              <li><a (click)= "onGenreChange('CFAGM')" >Cinimatic-Folk-Avant-Garde-Metal</a></li>
+            </ul>
+          </li>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sort By Artist<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a (click)= "onArtistChange('all')" >All Genres</a></li>
+              <li><a (click)= "onArtistChange('Pink Floyd')" >Pink Floyd</a></li>
+              <li><a (click)= "onArtistChange('Grateful Dead')" >Grateful Dead</a></li>
+              <li><a (click)= "onArtistChange('Hella')" >Hella</a></li>
+              <li><a (click)= "onArtistChange('Grails')" >Grails</a></li>
+              <li><a (click)= "onArtistChange('Busdriver')" >Busdriver</a></li>
+              <li><a (click)= "onArtistChange('Aesop Rock')" >Aesop Rock</a></li>
+              <li><a (click)= "onArtistChange('Bjork')" >Bjork</a></li>
+              <li><a (click)= "onArtistChange('Mike Patton')" >Mike Patton</a></li>
+              <li><a (click)= "onArtistChange('Secret Chiefs 3')" >Secret Chiefs 3</a></li>
+              <li><a (click)= "onArtistChange('Estradasphere')" >Estradasphere</a></li>
+          </ul>
         </div>
         <div class="navbar-header navbar-right">
 
@@ -53,39 +110,9 @@ import { User } from './user.model';
 })
 
 export class AppComponent {
-  public masterCdList: Cd [] = [
-    new Cd("Pink Floyd", "Dark Side of the Moon", "Psychadelic-Rock", 10, "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png"),
-    new Cd("Pink Floyd", "Ummagumma", "Psychadelic-Rock", 9, "https://upload.wikimedia.org/wikipedia/en/1/16/PinkFloyd-album-ummagummastudio-300.jpg"),
-    new Cd("Pink Floyd", "The Wall", "Post-Psychadelic", 18, "https://lh6.googleusercontent.com/-g6CITlz9D2c/TXFDg1-Nt9I/AAAAAAAABOU/e11x59aTi3o/s1600/Pink_Floyd_-_The_Wall.jpg"),
-    new Cd("Grateful Dead", "Dick's Picks vol. 102", "Psychadelic-Rock", 5, "http://www.artguychuck.com/wp-content/uploads/2013/09/dicksPicksRegional.jpg"),
-    new Cd("Grateful Dead", "American Beauty", "Psychadelic-Rock", 8, "http://www.covermesongs.com/wp-content/uploads/2016/03/americanbeauty.jpg"),
-    new Cd("Grateful Dead", "Grateful Dead(Self-Titled)", "Accapella-Experimental", 7, "http://www.dead.net/sites/default/files/images/DECD132_0.jpg"),
-    new Cd("Hella", "Tripper", "Post-Psychadelic", 7, "https://f4.bcbits.com/img/a0193062514_10.jpg"),
-    new Cd("Hella", "Hold Your Horse Is", "Post-Psychadelic", 9, "https://f4.bcbits.com/img/a2970431076_16.jpg"),
-    new Cd("Hella", "There Is No 666 in Outerspace", "Post-Psychadelic", 11, "https://f4.bcbits.com/img/a3972258467_16.jpg"),
-    new Cd("Grails", "Deep Politics", "Post-Psychadelic", 8, "http://www.brooklynvegan.com/files/img/metal/various/grailscover.jpg"),
-    new Cd("Grails", "Take Refuge In Clean Living", "Post-Psychadelic", 12, "http://www.progarchives.com/progressive_rock_discography_covers/3395/cover_304191122014_r.JPG"),
-    new Cd("Grails", "Burning Off Impurities", "Post-Psychadelic", 10, "https://images-na.ssl-images-amazon.com/images/I/41sK7Fp4V5L.jpg"),
-    new Cd("Busdriver", "Beaus and Eros", "Accapella-Experimental", 12, "http://a2.mzstatic.com/us/r30/Music2/v4/0c/25/dc/0c25dca4-5530-6391-c4d6-3575b6df9fa6/669158523665.1200x1200-75.jpg"),
-    new Cd("Busdriver", "Perfect Hair", "Cinimatic-Folk-Avant-Garde-Metal", 20, "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Perfect_hair_album_cover.jpg/220px-Perfect_hair_album_cover.jpg"),
-    new Cd("Busdriver", "Roadkill Overcoat", "Avant-Garde-Rap", 13, "http://cdn3.pitchfork.com/albums/9656/homepage_large.73b286fa.jpg"),
-    new Cd("Aesop Rock", "Labor Days", "Avant-Garde-Rap", 11, "http://images.rapgenius.com/4oasqz2d03mhvndzvj2mkm7qm.600x596x1.jpg"),
-    new Cd("Aesop Rock", "The Impossible Kid", "Avant-Garde-Rap", 18, "https://upload.wikimedia.org/wikipedia/en/6/6a/The_Impossible_Kid_Album_Cover.jpg"),
-    new Cd("Aesop Rock", "Skelethon", "Avant-Garde-Rap", 14, "http://images.rapgenius.com/3bb1c2e5bd56f3448a01aef2505b6e07.608x608x1.jpg"),
-    new Cd("Bjork", "Medula", "Accapella-Experimental", 10, "https://upload.wikimedia.org/wikipedia/en/9/98/Medulla.jpg"),
-    new Cd("Bjork", "Homogenic", "Accapella-Experimental", 15, "https://upload.wikimedia.org/wikipedia/en/0/08/Bjork_-_Homogenic_album_cover.jpg"),
-    new Cd("Bjork", "Post", "Accapella-Experimental", 16, "http://static.stereogum.com/uploads/2015/06/Bjork-Post.jpg"),
-    new Cd("Mike Patton", "Laborintus II", "Accapella-Experimental", 15, "https://upload.wikimedia.org/wikipedia/en/e/e1/Laborintus_II.jpg"),
-    new Cd("Mike Patton", "Mondo Cane", "Avant-Garde-Rap", 16, "https://www.rarerecords.com.au/wp-content/uploads/2016/08/Mike-Patton-Mondo-Cain.jpg"),
-    new Cd("Secret Chiefs 3", "Path of Most Resistance", "Cinimatic-Folk-Avant-Garde-Metal", 12, "https://upload.wikimedia.org/wikipedia/en/5/59/Secret_Chiefs_3_Path_of_Most_Resistance_Cover.jpg"),
-    new Cd("Secret Chiefs 3", "Ishraqiyun: Perichoresis ", "Cinimatic-Folk-Avant-Garde-Metal", 18, "https://e.snmc.io/lk/f/l/cfa8692fa340c05b956990fe915b9cff/5421410.jpg"),
-    new Cd("Secret Chiefs 3", "Book Of Horizons", "Cinimatic-Folk-Avant-Garde-Metal", 19, "http://www.progarchives.com/progressive_rock_discography_covers/2088/cover_2315317102010.jpg"),
-    new Cd("Estradasphere", "Quadrapus", "Cinimatic-Folk-Avant-Garde-Metal", 20, "https://upload.wikimedia.org/wikipedia/en/3/38/Quadropus.jpg"),
-    new Cd("Estradasphere", "Palace Of Mirrors", "Cinimatic-Folk-Avant-Garde-Metal", 13, "http://cps-static.rovicorp.com/3/JPG_1080/MI0000/641/MI0000641631.jpg?partner=allrovi.com"),
-    new Cd("Estradasphere", "Passion For Life", "Avant-Garde-Rap", 17, "https://upload.wikimedia.org/wikipedia/en/1/14/Estradasphere-Passion_for_Life.jpg"),
+  public cdList = new MasterCdList();
+  public masterCdList: Cd[] = this.cdList.masterCdList;
 
-
-  ]
   public user = new User();
   public desiredGenre: string = "all";
   public desiredArtist: string = "all";
@@ -102,5 +129,8 @@ export class AppComponent {
     this.user.orderTotal += cdToBeAdded.price;
     console.log(this.user.albums);
     console.log(this.user.orderTotal);
+  }
+  doStuff(){
+    console.log("test");
   }
 }
